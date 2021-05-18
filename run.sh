eefc7cec0825ea36cf3978d64ebbdbceb9c8dd70
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2021-05-18 11:20:21 +0300

    readme, run.sh, license

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -e
+
+build() {
+  docker build -t ma3obblu/tarantool-luatest:2.6.2 .
+}
+
+push() {
+  docker tag ma3obblu/tarantool-luatest:2.6.2 ma3obblu/tarantool-luatest:2.6.2
+  docker push ma3obblu/tarantool-luatest:2.6.2
+}
+
+build_and_push() {
+  build
+  push
+}
+
+# Добавьте сюда список команд
+using(){
+  echo "Укажите команду при запуске: ./run.sh [command]"
+  echo "Список команд:"
+  echo "  build - запустить сборку локального образа"
+  echo "  push - проставить тег и выгрузить образ на docker hub"
+  echo "  build_and_push - собрать локальный образ, поставить тег и выгрузить на docker hub"
+}
+
+command="$1"
+if [ -z "$command" ]
+then
+ using
+ exit 0;
+else
+ $command $@
+fi
